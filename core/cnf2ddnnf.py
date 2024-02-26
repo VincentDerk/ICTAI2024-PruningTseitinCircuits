a286b9ffee69bcd1905b0179672c70f7f92a4672
VincentDerk <vincent.derkinderen@cs.kuleuven.be>
2024-02-26 17:57:25 +0100

    Added support for compiling using d4

--- core/cnf2ddnnf.py
+++ core/cnf2ddnnf.py
@@ -8,24 +8,34 @@ from core.cnf import CNF
 from core.ddnnf import DDNNF
 
 
-def cnf_to_ddnnf(cnf: CNF, k="dsharp") -> DDNNF:
-    return _compile_with_dsharp(cnf, smooth=True)
+def cnf_to_ddnnf(cnf: CNF, k="D4") -> DDNNF:
+    if k == "dsharp":
+        return _compile_with_dsharp(cnf, smooth=True)
+    else:
+        return _compile_with_d4(cnf)
 
 
-def _compile_with_dsharp(cnf: CNF, smooth=True):
+def _compile_with_d4(cnf: CNF):
     result = None
     fd1, cnf_file = tempfile.mkstemp(".cnf")
     fd2, nnf_file = tempfile.mkstemp(".nnf")
     os.close(fd1)
     os.close(fd2)
-    if smooth:
-        smoothl = ["-smoothNNF"]
-    else:
-        smoothl = []
-    cmd = ["dsharp", "-Fnnf", nnf_file] + smoothl + ["-disableAllLits", cnf_file]  #
+    cmd = ["../bin/d4_static", cnf_file, "-dDNNF", f"-out={nnf_file}"]
 
     try:
-        result = _compile(cnf, cmd, cnf_file, nnf_file)
+        # write dimacs
+        with open(cnf_file, "w") as f:
+            f.write(cnf.to_dimacs())
+
+        # call compiler
+        result = subprocess.run(cmd, shell=False, check=True, stdout=subprocess.DEVNULL)
+        success = result.returncode == 0
+        if not success:
+            raise Exception(f"D4 subprocess failed {cmd}")
+
+        # load result
+        result = _load_nnf_d4(nnf_file, cnf.var_count)
     except subprocess.CalledProcessError as err:
         raise err
     finally:
@@ -40,23 +50,179 @@ def _compile_with_dsharp(cnf: CNF, smooth=True):
     return result
 
 
-def _compile(cnf: CNF, cmd, cnf_file: str, nnf_file: str):
-    assert cnf.clause_count() > 0
-    with open(cnf_file, "w") as f:
-        f.write(cnf.to_dimacs())
+def _load_nnf_d4(filepath: str, num_vars) -> DDNNF:
+    # we assume node 1 is the root node.
+    # each node has a node_type definition, appearing before listing its edges.
+    with open(filepath) as f:
+        # first we create an intermediate structure of D4's NNF graph,
+        # combining the information of each or/and node.
+        nnf_graph_d4 = [None]
+        node_types = [None]
+
+        def _add_node_type(_node_type, _node_idx):
+            if _node_idx >= len(node_types):
+                extend_with_nb = _node_idx - len(node_types) + 1
+                node_types.extend((None for _ in range(extend_with_nb)))
+            node_types[_node_idx] = _node_type
+
+        def _add_or_node(_curr_node_idx, _child_node_idx, _implied_literals):
+            if _curr_node_idx >= len(nnf_graph_d4):
+                extend_with_nb = _curr_node_idx - len(nnf_graph_d4) + 1
+                nnf_graph_d4.extend((None for _ in range(extend_with_nb)))
+            if nnf_graph_d4[_curr_node_idx] is None:
+                nnf_graph_d4[_curr_node_idx] = list()
+            nnf_graph_d4[_curr_node_idx].append((_child_node_idx, _implied_literals))
+
+        def _add_and_node(_curr_node_idx, _child_node_idx):
+            if _curr_node_idx >= len(nnf_graph_d4):
+                extend_with_nb = _curr_node_idx - len(nnf_graph_d4) + 1
+                nnf_graph_d4.extend((None for _ in range(extend_with_nb)))
+            if nnf_graph_d4[_curr_node_idx] is None:
+                nnf_graph_d4[_curr_node_idx] = list()
+            nnf_graph_d4[_curr_node_idx].append(child_node_idx)
+
+        for line in f.readlines():
+            print(line)
+            line = line[:-2].strip().split()
+            node_type = line[0]
+            is_node_def = node_type in ("o", "a", "t", "f")
+            if is_node_def:
+                node_idx = int(line[1])
+                _add_node_type(node_type, node_idx)
+            else:
+                node_idx = int(line[0])
+                if node_types[node_idx] == "o":
+                    curr_node_idx = int(line[0])
+                    child_node_idx = int(line[1])
+                    implied_lits = {int(lit) for lit in line[2:]}
+                    assert all(abs(lit) <= num_vars for lit in implied_lits)
+                    _add_or_node(curr_node_idx, child_node_idx, implied_lits)
+                else:
+                    assert len(line) == 2
+                    curr_node_idx = int(line[0])
+                    child_node_idx = int(line[1])
+                    _add_and_node(curr_node_idx, child_node_idx)
+
+        # now we create the ddnnf recursively.
+        node_map = [None] * len(nnf_graph_d4)
+        ddnnf = DDNNF()
+        for atom in range(1, num_vars+1):
+            ddnnf.add_atom(atom)
+
+        # recursive DFS annotation starting from node 1
+        def _recursive_complete_ddnnf(d4_idx):
+            if node_map[d4_idx] is not None:  # check cache
+                return node_map[d4_idx]
+
+            node_type = node_types[d4_idx]
+            if node_type == "t":
+                return -1
+            elif node_type == "f":
+                return 0
+            elif node_type == "o":
+                node_info = nnf_graph_d4[d4_idx]
+                if len(node_info) == 1:
+                    child, implied_literals = node_info[0]
+                    assert len(implied_literals) == 0
+                    return _recursive_complete_ddnnf(child)
+
+                assert len(node_info) == 2
+                child1, implied_literals1 = node_info[0]
+                child2, implied_literals2 = node_info[1]
+                mapped_child1 = _recursive_complete_ddnnf(child1)
+                mapped_child2 = _recursive_complete_ddnnf(child2)
+
+                # if any child is True; replace with True
+                # if any child is False; replace with the other child
+                # TODO: Then we might need to clean up previous nodes?
+
+                # create and node 1
+                if mapped_child1 > 0:
+                    children1 = (mapped_child1,) + tuple(implied_literals1)
+                else:
+                    assert mapped_child1 == -1, "Did not expect false."
+                    children1 = tuple(implied_literals1)
+                if len(children1) == 1:
+                    child_ddnnf_node1 = children1[0]
+                else:
+                    child_ddnnf_node1 = ddnnf.add_conj(children1)
+
+                # create and node 2
+                if mapped_child2 > 0:
+                    children2 = (mapped_child2,) + tuple(implied_literals2)
+                else:
+                    assert mapped_child2 == -1, "Did not expect false"
+                    children2 = tuple(implied_literals2)
+                if len(children2) == 1:
+                    child_ddnnf_node2 = children2[0]
+                else:
+                    child_ddnnf_node2 = ddnnf.add_conj(children2)
+                # combine and nodes into or node
+                or_ddnnf_node = ddnnf.add_disj((child_ddnnf_node1, child_ddnnf_node2))
+                node_map[d4_idx] = or_ddnnf_node
+                return or_ddnnf_node
+
+            elif node_type == "a":
+                # create and node and return it. We assume none is "True"
+                children = nnf_graph_d4[d4_idx]
+                assert len(children) > 1
+                mapped_children = []
+                for child in children:
+                    mapped_child = _recursive_complete_ddnnf(child)
+                    assert mapped_child > 0
+                    mapped_children.append(mapped_child)
+                and_node_ddnnf = ddnnf.add_conj(tuple(mapped_children))
+                node_map[d4_idx] = and_node_ddnnf
+                return and_node_ddnnf
+
+        final_idx = _recursive_complete_ddnnf(1)
+        # TODO: if final_idx is 0 then ddnnf must represent False.
+        # TODO: if final_idx is -1, then ddnnf must represent True.
+        assert final_idx > 0
+        return ddnnf
+
+
+def _compile_with_dsharp(cnf: CNF, smooth=True):
+    result = None
+    fd1, cnf_file = tempfile.mkstemp(".cnf")
+    fd2, nnf_file = tempfile.mkstemp(".nnf")
+    os.close(fd1)
+    os.close(fd2)
+    if smooth:
+        smoothl = ["-smoothNNF"]
+    else:
+        smoothl = []
+    cmd = ["../bin/dsharp", "-Fnnf", nnf_file] + smoothl + ["-disableAllLits", cnf_file]  #
 
     try:
+        # write dimacs
+        with open(cnf_file, "w") as f:
+            f.write(cnf.to_dimacs())
+
+        # call compiler
         result = subprocess.run(cmd, shell=False, check=True, stdout=subprocess.DEVNULL)
         success = result.returncode == 0
         if not success:
             raise DSharpError()
+
+        # load result
+        result = _load_nnf(nnf_file)
     except subprocess.CalledProcessError as err:
-        print(err)
         raise err
-    return _load_nnf(nnf_file)
+    finally:
+        try:
+            os.remove(cnf_file)
+        except OSError:
+            pass
+        try:
+            os.remove(nnf_file)
+        except OSError:
+            pass
+    return result
 
 
 def _load_nnf(filename: str) -> DDNNF:
+    """ NNF from dsharp """
     nnf = DDNNF()
 
     with open(filename) as f:
@@ -116,3 +282,4 @@ def _load_nnf(filename: str) -> DDNNF:
                     node_idx = cache_value
                 line2idx[line_nr] = node_idx
     return nnf
+
