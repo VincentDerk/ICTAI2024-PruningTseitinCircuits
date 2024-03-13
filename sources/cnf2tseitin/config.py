4ae65c905c577c5cc1f524e542774bbd13b23f7b
VincentDerk <vincent.derkinderen@cs.kuleuven.be>
2024-03-13 11:18:20 +0100

    Added config file, might not be needed anymore though..

---
+++ sources/cnf2tseitin/config.py
@@ -0,0 +1,8 @@
+model_counting_folders = [
+    "../sources/cnf2tseitin/cnf/MC2022_track1-mc_public/",
+    "../sources/cnf2tseitin/cnf/MC2022_track2-wmc_public/",
+    "../sources/cnf2tseitin/cnf/MC2023_track1-mc-private/",
+    "../sources/cnf2tseitin/cnf/MC2023_track1-mc_public/",
+    "../sources/cnf2tseitin/cnf/MC2023_track2-wmc_private/",
+    "../sources/cnf2tseitin/cnf/MC2023_track2-wmc_public/"
+]
